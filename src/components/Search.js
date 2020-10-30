6d57b0371fa45a9ec0ed16e50473f43a334dedc3
Brandan Sayarath <brandan@brandan.tech>
2020-10-30 14:27:09 -0700

    fixed expression error on await axios line

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,36 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
 
-  const [term, setTerm] = useState('');
-
-  console.log('I run with every render and at initial render!')
+  console.log("I run with every render and at initial render!");
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php"), {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
-          action: 'query',
-          list: 'search',
-          orgin: '*',
-          format: 'json',
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
           srsearch: term,
-        }
-      };
+        },
+      });
     };
-
     search();
-
-  }, [term])
+  }, [term]);
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term:</label>
-          <input 
-          value={term}
-          onChange={e => setTerm(e.target.value)}
-          className="input" />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
     </div>
@@ -40,4 +38,3 @@ const Search = () => {
 };
 
 export default Search;
-
