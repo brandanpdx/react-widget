026144b9767450500f41404db3d0de1ec02582b6
Brandan Sayarath <brandan@brandan.tech>
2020-10-29 23:09:43 -0700

    add accordion content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import Accordion from './/components/Accordion';
+import React from "react";
+import Accordion from ".//components/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a Frontend Framework!",
+  },
+  {
+    title: "Why use React?",
+    content: "React is cool, that's why!",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by using Components!",
+  },
+];
 
 export default () => {
-  return <div>
-    <Accordion/>
-  </div>
-}
-
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />
+    </div>
+  );
+};
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Accordion = () => {
-  return <h1>Accordion</h1>
-}
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active">
+          <icon className="dropdown icon"></icon>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">
+    {renderedItems}
+  </div>
+};
 
 export default Accordion;
