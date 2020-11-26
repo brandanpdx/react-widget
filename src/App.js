536144f7b5aba8a970d3b2ba28be6cf0be7001ec
Brandan Sayarath <brandan@brandan.tech>
2020-11-25 17:55:56 -0800

    fix props in app.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
 import React, { useState } from "react";
-import Accordion from ".//components/Accordion";
-import Search from ".//components/Search";
-import Dropdown from ".//components/Dropdown";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
     title: "What is React?",
-    content: "React is a Frontend Framework!",
+    content: "React is a front end javascript framework",
   },
   {
     title: "Why use React?",
-    content: "React is cool, that's why!",
+    content: "React is a favorite JS library among engineers",
   },
   {
     title: "How do you use React?",
-    content: "You use React by using Components!",
+    content: "You use React by creating components",
   },
 ];
 
 const options = [
   {
-    label: 'The Color Red',
-    value: 'red'
+    label: "The Color Red",
+    value: "red",
   },
   {
-    label: 'The Color Green',
-    value: 'blue'
+    label: "The Color Green",
+    value: "green",
   },
   {
-    label: 'A Shade of Blue',
-    value: 'blue'
-  }
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
-
-const App = () => {
-
+export default () => {
   const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Dropdown 
-      selected={selected} 
-      onSelectedChange={setSelected}
-      options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
-
-export default App;
