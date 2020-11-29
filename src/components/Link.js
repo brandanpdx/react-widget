d26d15315ac5af560e3e2a84e00c476b72785f6a
Brandan Sayarath <brandan@brandan.tech>
2020-11-28 20:37:13 -0800

    add Link component

---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Link = ({ href, className, children }) => {
+
+  const onClick = (event) => {
+    event.preventDefault();
+    window.history.pushState({}, '', href)
+  }
+
+
+  return (
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
