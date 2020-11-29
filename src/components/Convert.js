50f0596621454702ff8a0506906c3acc81acab02
Brandan Sayarath <brandan@brandan.tech>
2020-11-28 18:54:50 -0800

    add debouncing to translation app

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,27 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ lang, text }) => {
-    const [translated, setTranslated] = useState('')
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
 
-    useEffect(() => {
-        const doTranslation = async () => {
-            const { data } = await axios
-                .post('https://translation.googleapis.com/language/translate/v2', {}, {
-                    params: {
-                        q: text,
-                        target: lang.value,
-                        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-                    }
-                })
-                setTranslated(data.data.translations[0].translatedText)
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: lang.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
         }
-        doTranslation();
-    }, [lang, text])
-    return (
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [lang, debouncedText]);
+  return (
     <div>
-        <h1 className="ui header">{translated}</h1>
-    </div>)
-}
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
+};
 
 export default Convert;
