eb0223315d8f18782be4189307506c430e6e7696
Brandan Sayarath <brandan@brandan.tech>
2020-11-28 20:43:02 -0800

    add navEvent in Link.js and eventListener in Route.js

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Link = ({ href, className, children }) => {
-
   const onClick = (event) => {
     event.preventDefault();
-    window.history.pushState({}, '', href)
-  }
+    window.history.pushState({}, "", href);
 
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
 
   return (
     <a onClick={onClick} className={className} href={href}>
